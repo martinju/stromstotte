3ba105ef7dc20c120de588ba58ba74031aeb4e0c
Martin <jullum@nr.no>
2022-09-28 22:21:49 +0200

    more compensation

--- scripts/viz_compensation.R
+++ scripts/viz_compensation.R
@@ -28,4 +28,37 @@ quantiles_dt[,quant:=lapply(type,get_quant)]
 
 #### Plotting compensation
 
-ggplot(density_dt[type=="compensation"],aes(x=x,y=y))+geom_line()+facet_wrap(vars(area))
+#ggplot(density_dt[type=="compensation"],aes(x=x,y=y))+geom_line()+facet_wrap(vars(area))
+
+tab=NULL
+for(j in seq_along(areas)){
+  mp_m <- format(round(res_dt[area==areas[j] & type=="mean",mean_price],2),nsmall=2)
+  mp_CI_L <- format(round(res_dt[area==areas[j] & type=="quantile_0.025",mean_price],2),nsmall=2)
+  mp_CI_U <- format(round(res_dt[area==areas[j] & type=="quantile_0.975",mean_price],2),nsmall=2)
+
+  mp_lb <- format(round(res_dt[area==areas[j] & type=="lower_bound",mean_price],2),nsmall=2)
+  mp_cm <- format(round(res_dt[area==areas[j] & type=="current_mean",mean_price],2),nsmall=2)
+
+
+  c_m <- format(round(res_dt[area==areas[j] & type=="mean",compensation],2),nsmall=2)
+  c_CI_L <- format(round(res_dt[area==areas[j] & type=="quantile_0.025",compensation],2),nsmall=2)
+  c_CI_U <- format(round(res_dt[area==areas[j] & type=="quantile_0.975",compensation],2),nsmall=2)
+
+  c_lb <- format(round(res_dt[area==areas[j] & type=="lower_bound",compensation],2),nsmall=2)
+  c_cm <- format(round(res_dt[area==areas[j] & type=="current_mean",compensation],2),nsmall=2)
+
+  tab0 <- cbind(c("Gjennomsnittspris",mp_m,
+                  paste0("(",mp_CI_L,",",mp_CI_U,")"),
+                  mp_lb,
+                  mp_cm),
+                c("Kompensasjon",c_m,
+                  paste0("(",c_CI_L,",",c_CI_U,")"),
+                  c_lb,
+                  c_cm))
+
+  tab <- cbind(tab,tab0)
+}
+
+colnames(tab) <- rep(areas,each=2)
+rownames(tab) <- c("","Estimat","95% konfidens","Absolutt nedre grense","Saa langt denne maned")
+
