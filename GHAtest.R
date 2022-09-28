b31899dd178b0d1acc0f4d3b961b38ccdbc4343a
Martin <jullum@nr.no>
2022-09-28 22:35:48 +0200

    gha-test

---
+++ GHAtest.R
@@ -0,0 +1,12 @@
+
+
+library(data.table)
+library(ggplot2)
+
+dt <- data.table(aa=1:10,bb=rnorm(10))
+dt[,systime:=Sys.time()]
+
+ggplot(dt,aes(x=aa,y=bb))+geom_line()
+
+fwrite("GHA_testing.csv")
+
