3334f8203e0176077663a3b1e19fbd9de7c1e8f6
Martin <jullum@nr.no>
2022-11-04 10:59:17 +0100

    testing shiny

--- shiny/app.R
+++ shiny/app.R
@@ -7,28 +7,77 @@
 #    http://shiny.rstudio.com/
 #
 
+## Sidepanels
+# Velg fylke
+# Velg nettleverandør
+# 1. Strømpris NÅ
+# 2. Estimert strømstøtte
+# 3. Estimert strømpris
+
+
+
+library(data.table)
+
+dt_nettleie <- fread("../data/database_nettleie_simple.csv")
+dt_nettleie_kl6 <- fread("../data/database_nettleie_simple_kl_6.csv")
+dt_hourly <- fread("../data/database_nordpool_hourly.csv")
+dt_comp <- fread("../data/current_estimated_compensation.csv")
+
+
 library(shiny)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
-  verbatimTextOutput("summary"),
-  tableOutput("table")
+## app.R ##
+library(shinydashboard)
+
+header <- dashboardHeader(title = "Estimert reell strømpris",titleWidth = 300)
+
+sidebar <- dashboardSidebar(
+  width = 300,
+  sidebarMenu(
+    menuItem("Min strømpris", tabName = "dashboard", icon = icon("dashboard")),
+    selectInput("fylkevalg","Velg Fylke",dt_nettleie[,unique(Fylke)]),
+    menuItem("Historisk estimering", tabName = "historic", icon = icon("th")),
+    menuItem("Metodikk", icon = icon("file-code-o"),
+             href = "https://martinjullum.com/sideprojects/stromstotte")
+  )
+
 )
-# Define server logic required to draw a histogram
+
+body <-  dashboardBody(
+  tabItems(
+    # First tab content
+    tabItem(tabName = "dashboard",
+            fluidRow(
+              box(selectInput("fylkevalg","Velg Fylke",dt_nettleie[,unique(Fylke)])),
+              box(plotOutput("plot1", height = 250)),
+              box(
+                title = "Controls",
+                sliderInput("slider", "Number of observations:", 1, 100, 50)
+              )
+            )
+    ),
+
+    # Second tab content
+    tabItem(tabName = "historic",
+            h2("Putt inn et eller annet her.")
+    )
+  )
+)
+
+
+ui <- dashboardPage(header, sidebar, body)
+
 server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
 
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white',
-         xlab = 'Waiting time to next eruption (in mins)',
-         main = 'Histogram of waiting times')
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
   })
+
+  output$dt_nettleie <- dt_nettleie[Fylke==input$fylkevalg]
 }
 
-# Run the application
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
