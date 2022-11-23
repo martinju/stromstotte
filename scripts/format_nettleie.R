5c0ccca67d068e96d05fe73e9e83e7f4d35ba81e
Martin <jullum@nr.no>
2022-11-23 12:17:47 +0100

    nettleie fixes

--- scripts/format_nettleie.R
+++ scripts/format_nettleie.R
@@ -11,7 +11,8 @@ nettleie[,length(unique(Energiledd)),by=.(Nettselskap,Time,Fylke)][,any(V1!=1)]
 
 setnames(nettleie,"Har MVA","MVA")
 
-keep_cols <- c("Nettselskap","Fylke", "MVA", "Energiledd","Time")
+#keep_cols <- c("Nettselskap","Fylke", "MVA", "Energiledd","Time")
+keep_cols <- c("Nettselskap","Fylke", "Energiledd","Time")
 
 nettleie_dt <- unique(nettleie[,..keep_cols])
 
@@ -27,44 +28,59 @@ nettleie_dt[Energiledd_diff!=0&Time==7]
 nettleie_dt[,pristype:="Natt"]
 nettleie_dt[Time%in% seq(6,21),pristype:="Dag"]
 
-nettleie_dt_simple <- unique(nettleie_dt[Time %in% c(0,10),.(Fylke,Nettselskap,Energiledd,pristype,MVA)])
+nettleie_dt_simple <- unique(nettleie_dt[Time %in% c(0,10),.(Fylke,Nettselskap,Energiledd,pristype)])
 
 # Special object to handle FØIE AS that differs from the others in terms of definition of day/night (night include 6-7 as well)
-nettleie_dt_simple_kl_6 <- unique(nettleie_dt[Time %in% 6,.(Fylke,Nettselskap,Energiledd,Time,pristype,MVA)])
+nettleie_dt_simple_kl_6 <- unique(nettleie_dt[Time %in% 6,.(Fylke,Nettselskap,Energiledd,Time,pristype)])
 
 setkey(nettleie_dt_simple,Fylke,Nettselskap,pristype)
 setkey(nettleie_dt_simple_kl_6,Fylke,Nettselskap,pristype)
+
+### CHecking if Fylke matters at all
+
+(fylke_matters1 <- unique(nettleie_dt_simple[,.(Nettselskap,Energiledd,pristype)])[,.N,by=Nettselskap][,any(N!=2)]) # FALSE
+(fylke_matters2 <- unique(nettleie_dt_simple_kl_6[,.(Nettselskap,Energiledd,pristype)])[,.N,by=Nettselskap][,any(N!=1)]) # FALSE
+
+if(!(fylke_matters1 & fylke_matters1)){
+  nettleie_dt_simple[,Fylke:=NULL]
+  nettleie_dt_simple_kl_6[,Fylke:=NULL]
+
+  nettleie_dt_simple <- unique(nettleie_dt_simple)
+  nettleie_dt_simple_kl_6 <- unique(nettleie_dt_simple_kl_6)
+
+}
+
 
 fwrite(nettleie_dt_simple,"data/database_nettleie_simple.csv")
 fwrite(nettleie_dt_simple_kl_6,"data/database_nettleie_simple_kl_6.csv")
 
 
-### Testing
-
-
-nettleie_dt_simple[Nettselskap=="LEDE AS"]
-
-
-nettleie_dt_simple[Nettselskap=="FØIE AS"]
-nettleie_dt_simple_kl_6[Nettselskap=="FØIE AS"]
-
-nettleie_dt[Nettselskap=="FØIE AS"]
-
-nettleie_dt[Nettselskap=="NORGESNETT AS"]
-
-
-nettleie_dt[,.N,by=.(Nettselskap)][N!=24]
-
-
-nettleie_dt[Nettselskap=="GLITRE ENERGI NETT AS" & Time==0]
-
-nettleie_dt[Nettselskap=="UVDAL KRAFTFORSYNING SA"]
-
-
-nettleie_dt[,unique(Fylke)]
-
-nettleie_dt[Fylke=="Vestfold og Telemark",unique(Nettselskap)]
-
-nettleie_dt[,.N,by=c("Nettselskap","Effekttrinn fra KW")]
-
-nettleie_dt[Nettselskap=="FJELLNETT AS" & Time==3 & `Effekttrinn fra KW` == 21]
+# ### Testing
+#
+#
+# nettleie_dt_simple[Nettselskap=="LEDE AS"]
+#
+#
+# nettleie_dt_simple[Nettselskap=="FØIE AS"]
+# nettleie_dt_simple_kl_6[Nettselskap=="FØIE AS"]
+#
+# nettleie_dt[Nettselskap=="FØIE AS"]
+#
+# nettleie_dt[Nettselskap=="NORGESNETT AS"]
+#
+#
+# nettleie_dt[,.N,by=.(Nettselskap)][N!=24]
+#
+#
+# nettleie_dt[Nettselskap=="GLITRE ENERGI NETT AS" & Time==0]
+#
+# nettleie_dt[Nettselskap=="UVDAL KRAFTFORSYNING SA"]
+#
+#
+# nettleie_dt[,unique(Fylke)]
+#
+# nettleie_dt[Fylke=="Vestfold og Telemark",unique(Nettselskap)]
+#
+# nettleie_dt[,.N,by=c("Nettselskap","Effekttrinn fra KW")]
+#
+# nettleie_dt[Nettselskap=="FJELLNETT AS" & Time==3 & `Effekttrinn fra KW` == 21]
