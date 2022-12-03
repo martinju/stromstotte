0848e0d3c1e68a1b5c6ddbf58947d1410b8ed390
Martin <jullum@nr.no>
2022-12-03 23:29:01 +0100

    fungerende hover - kun småfiks gjenstår

--- shiny/devel_plot_naa.R
+++ shiny/devel_plot_naa.R
@@ -1,3 +1,12 @@
+# TODO:
+# Fix header conflict with buttons for history
+# Fjern "estimat" fra text der strømstøtten er kjent.
+# Fiks farger i text slik at de passer med dem for
+# Endre legend title fra "type" til noe annet
+# Legg now inn i min strømpris nå, og tilsvarende history inn i historiske reelle strømpriser
+
+
+
 textfunc <- function(datetime,spotpris,nettleie,totalpris,totalpris_lower_CI,totalpris_upper_CI,stotte,stotte_lower_CI,stotte_upper_CI){
   date <- as.Date(datetime)
   start_hour <- lubridate::hour(datetime)
@@ -54,14 +63,17 @@ plot_strompris_naa_dt[,datetime:=as.POSIXct(date)+start_hour*60*60]
 
 plot_strompris_naa_dt0 <- plot_strompris_naa_dt[,.(datetime,spotpris,nettleie,totalpris,totalpris_lower_CI,totalpris_upper_CI,stotte,stotte_lower_CI,stotte_upper_CI)]
 
+texthelper_dt <- plot_strompris_naa_dt0[,.(datetime,text=textfunc(datetime,spotpris,nettleie,totalpris,totalpris_lower_CI,totalpris_upper_CI,stotte,stotte_lower_CI,stotte_upper_CI))]
+
 plot_strompris_naa_dt0_dup <- copy(plot_strompris_naa_dt0)
 plot_strompris_naa_dt0_dup[,datetime:=datetime+1*60*60-1]
 
 plot_strompris_naa_dt00 <- rbind(plot_strompris_naa_dt0,plot_strompris_naa_dt0_dup)
 setkey(plot_strompris_naa_dt00,datetime)
+#plot_strompris_naa_dt00[,null:=0]
 
-plot_strompris_naa_dt00[lubridate::second(datetime)==0,text:=textfunc(datetime,spotpris,nettleie,totalpris,totalpris_lower_CI,totalpris_upper_CI,stotte,stotte_lower_CI,stotte_upper_CI)]
-plot_strompris_naa_dt00[,text:=textfunc(datetime,spotpris,nettleie,totalpris,totalpris_lower_CI,totalpris_upper_CI,stotte,stotte_lower_CI,stotte_upper_CI)]
+#texthelper_dt[,type:="null"]
+#plot_strompris_naa_dt00[,text:=textfunc(datetime,spotpris,nettleie,totalpris,totalpris_lower_CI,totalpris_upper_CI,stotte,stotte_lower_CI,stotte_upper_CI)]
 
 
 plot_strompris_naa_dt_ints_totalpris <- plot_strompris_naa_dt00[,.(datetime,lower_CI=totalpris_lower_CI,upper_CI=totalpris_upper_CI)]
@@ -72,13 +84,15 @@ plot_strompris_naa_dt_ints_stotte[,type:="stotte"]
 
 plot_strompris_naa_dt_ints <- rbind(plot_strompris_naa_dt_ints_totalpris,plot_strompris_naa_dt_ints_stotte)
 
-plot_strompris_naa_dt_melted <- melt(plot_strompris_naa_dt00[,.(datetime,spotpris,nettleie,totalpris,stotte,text)],
-                                     id.vars = c("datetime","text"),variable.name = "type",value.name = "pris")
+plot_strompris_naa_dt_melted <- melt(plot_strompris_naa_dt00[,.(datetime,spotpris,nettleie,totalpris,stotte)],
+                                     id.vars = c("datetime"),variable.name = "type",value.name = "pris")
 
 plot_dt_final <- merge(plot_strompris_naa_dt_melted,plot_strompris_naa_dt_ints,by=c("datetime","type"),all = T)
 
 plot_dt_final[,linesize := "b"]
 plot_dt_final[type%in% c("totalpris","stotte"),linesize := "a"]
+
+#plot_dt_final <- merge(plot_dt_final,texthelper_dt,by=c("datetime","type"),all = T)
 
 setcolorder(plot_dt_final,c("datetime","type","pris","lower_CI","upper_CI","linesize"))
 
@@ -94,11 +108,14 @@ p_history <- ggplot(data=plot_dt_final,mapping=aes(x=datetime,y=pris,col=type,fi
                    breaks=breaks_pretty(12),
                    minor_breaks = breaks_pretty(24),
                    labels = label_date_short(format = c("%Y", "", "%d.%b\n", "%H:%M\n"),sep=""))+ # TODO: Get Norwegian months
-  scale_size_manual(values=c("a" = 1,"b"=0.5))
+  scale_size_manual(values=c("a" = 1,"b"=0.5))+
+  guides(size="none")+
+  geom_line(data=texthelper_dt,aes(x=datetime,y=0,text=text),inherit.aes = F,size=0.00001)
+
 
 p_history
 
-ggp_history <- ggplotly(p_history,dynamicTicks = TRUE)
+ggp_history <- ggplotly(p_history,dynamicTicks = TRUE,tooltip = "text")
 ggp_history <- layout(
   ggp_history,
   hovermode = "x unified",
@@ -131,9 +148,11 @@ ggp_history <- layout(
 )
 
 ggp_history <- style(ggp_history,visible="legendonly",traces=c(3,7)) #trace=2 identified through plotly_json(ggp_history)
+ggp_history <- style(ggp_history,hoverinfo="none",traces=1:8)
 ggp_history
 
-p_now <- ggplot(data=plot_dt_final[datetime>=today-1],mapping=aes(x=datetime,y=pris,col=type,fill=type,text=text))+
+
+p_now <- ggplot(data=plot_dt_final[datetime>=today-1],mapping=aes(x=datetime,y=pris,col=type,fill=type))+
   geom_line(aes(size=linesize))+
   geom_ribbon(aes(ymin = lower_CI, ymax = upper_CI), alpha = 0.5)+
   ggtitle("Estimert reell strømpris")+
@@ -143,7 +162,8 @@ p_now <- ggplot(data=plot_dt_final[datetime>=today-1],mapping=aes(x=datetime,y=p
                    minor_breaks = breaks_pretty(24),
                    labels = label_date_short(format = c("%Y", "", "%d.%b\n", "%H:%M\n"),sep=""))+ # TODO: Get Norwegian months
   scale_size_manual(values=c("a" = 1,"b"=0.5))+
-  guides(size="none")
+  guides(size="none")+
+  geom_line(data=texthelper_dt[datetime>=today-1],aes(x=datetime,y=0,text=text),inherit.aes = F,size=0.00001)
 
 p_now
 
@@ -152,9 +172,9 @@ ggp_now <- layout(
   ggp_now,
   hovermode = "x unified"
   )
-ggp_now <- style(ggp_now,visible="legendonly",traces=c(3,7)) #trace=2 identified through plotly_json(ggp_history)
+ggp_now <- style(ggp_now,visible="legendonly",traces=c(3,7)) #trace=2 identified through plotly_json(ggp_now)
+ggp_now <- style(ggp_now,hoverinfo="none",traces=1:8)
 ggp_now
-
 
 
 style(ggplotly(p),visible="legendonly", traces = 2)
