0b467e79ec703d9dbf05e14b9d7cf302ae78b83e
Martin <jullum@nr.no>
2022-12-05 11:02:47 +0100

    adding to app (initial)

--- shiny/app.R
+++ shiny/app.R
@@ -59,6 +59,8 @@ if(deployed){
   path <- "../"
 }
 
+source(file.path(path,"shiny/helper_funcs.R"))
+
 dt_nettleie <- fread(file.path(path,"data/database_nettleie_simple.csv"))
 dt_nettleie_kl6 <- fread(file.path(path,"data/database_nettleie_simple_kl_6.csv"))
 dt_hourly <- fread(file.path(path,"data/database_nordpool_hourly.csv"))
@@ -77,11 +79,15 @@ dt_postnr_nettselskap_prisomraader_map[,prisomraade:=gsub(" ","",prisomraade,fix
 
 dt_nettleie[,Energiledd:=Energiledd/100]
 
-twodigits <- function(x){
-  format(round(x,2),nsmall=2)
-}
 
-scaleFUN <- function(x) sprintf("%.2f", x)
+#####
+
+# Colorblind friendly colors from here: http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/#a-colorblind-friendly-palette
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+mycols <- c(spotpris=cbbPalette[1],
+            nettleie = cbbPalette[2],
+            stotte = cbbPalette[4],
+            totalpris = cbbPalette[3])
 
 
 ######
@@ -130,7 +136,7 @@ sidebar <- dashboardSidebar(
 
 body_strompris_naa <- tabItem(tabName = "strompris_naa",
                               fluidPage(
-                                plotlyOutput("spotplot"),
+                                plotlyOutput("now_spotplot"),
                                 fluidRow(
                                   box(width = 8,
                                       h3("Forklaring"),
@@ -357,7 +363,121 @@ server <- function(input, output,session) {
      )
    })
 
+   plot_dt_final <- reactive({
+     estimation_date0=today#today-1
 
+     #updated_dt_nettleie0 <- dt_nettleie[Nettselskap == input$nettselskap]
+     #updated_dt_hourly0 <- dt_hourly[area ==input$prisomraade]
+     #updated_dt_comp0 <- dt_comp[area == input$prisomraade]
+
+     updated_dt_nettleie0 <- dt_nettleie[Nettselskap=="ELVIA AS"]
+     updated_dt_hourly0 <- dt_hourly[area=="NO1"]
+     updated_dt_comp0 <- dt_comp[area == "NO1"]
+
+     updated_dt_hourly0[,computation_year:=year(date)]
+     updated_dt_hourly0[,computation_month:=month(date)]
+
+
+
+     updated_dt_comp0[,last_est_date_per_month:=max(estimation_date),by=.(computation_year,computation_month)]
+     updated_dt_comp0_1 <- updated_dt_comp0[estimation_date==last_est_date_per_month & estimation_date!=estimation_date0 & type=="median"]
+     updated_dt_comp0_2 <- updated_dt_comp0[estimation_date==estimation_date0 & type %in% c("median","quantile_0.025","quantile_0.975","lower_bound")]
+
+     plot_strompris_naa_dt <- copy(updated_dt_hourly0)
+     setnames(plot_strompris_naa_dt,"price","spotpris")
+
+     dc1 <- dcast(updated_dt_comp0_1[,.(compensation,type,computation_year,computation_month)],formula= computation_year+computation_month~type,value.var="compensation")
+     dc2 <- dcast(updated_dt_comp0_2[,.(compensation,type,computation_year,computation_month)],formula= computation_year+computation_month~type,value.var="compensation")
+
+     setnames(dc1,"median","stotte")
+     setnames(dc2,c("median","quantile_0.025","quantile_0.975","lower_bound"),c("stotte","stotte_lower_CI","stotte_upper_CI","stotte_lower_bound"))
+
+     dc3 <- rbind(dc1,dc2,fill=TRUE)
+
+
+     plot_strompris_naa_dt[start_hour %in% seq(6,21),nettleie:=updated_dt_nettleie0[pristype=="Dag",Energiledd]]
+     plot_strompris_naa_dt[is.na(nettleie),nettleie:=updated_dt_nettleie0[pristype=="Natt",Energiledd]]
+
+     plot_strompris_naa_dt <- merge(plot_strompris_naa_dt,dc3,by=c("computation_year", "computation_month"))
+
+     plot_strompris_naa_dt[,totalpris:=spotpris+nettleie-stotte]
+     plot_strompris_naa_dt[,totalpris_lower_CI:=spotpris+nettleie-stotte_lower_CI]
+     plot_strompris_naa_dt[,totalpris_upper_CI:=spotpris+nettleie-stotte_upper_CI]
+
+
+     plot_strompris_naa_dt[,datetime:=as.POSIXct(date)+start_hour*60*60]
+
+
+     plot_strompris_naa_dt0 <- plot_strompris_naa_dt[,.(datetime,spotpris,nettleie,totalpris,totalpris_lower_CI,totalpris_upper_CI,stotte,stotte_lower_CI,stotte_upper_CI)]
+
+     texthelper_dt <- plot_strompris_naa_dt0[,.(datetime,text=textfunc(datetime,spotpris,nettleie,totalpris,totalpris_lower_CI,totalpris_upper_CI,stotte,stotte_lower_CI,stotte_upper_CI))]
+
+     plot_strompris_naa_dt0_dup <- copy(plot_strompris_naa_dt0)
+     plot_strompris_naa_dt0_dup[,datetime:=datetime+1*60*60-1]
+
+     plot_strompris_naa_dt00 <- rbind(plot_strompris_naa_dt0,plot_strompris_naa_dt0_dup)
+     setkey(plot_strompris_naa_dt00,datetime)
+
+
+     plot_strompris_naa_dt_ints_totalpris <- plot_strompris_naa_dt00[,.(datetime,lower_CI=totalpris_lower_CI,upper_CI=totalpris_upper_CI)]
+     plot_strompris_naa_dt_ints_totalpris[,type:="totalpris"]
+
+     plot_strompris_naa_dt_ints_stotte <- plot_strompris_naa_dt00[,.(datetime,lower_CI=stotte_lower_CI,upper_CI=stotte_upper_CI)]
+     plot_strompris_naa_dt_ints_stotte[,type:="stotte"]
+
+     plot_strompris_naa_dt_ints <- rbind(plot_strompris_naa_dt_ints_totalpris,plot_strompris_naa_dt_ints_stotte)
+
+     plot_strompris_naa_dt_melted <- melt(plot_strompris_naa_dt00[,.(datetime,spotpris,nettleie,totalpris,stotte)],
+                                          id.vars = c("datetime"),variable.name = "type",value.name = "pris")
+
+     plot_dt_final <- merge(plot_strompris_naa_dt_melted,plot_strompris_naa_dt_ints,by=c("datetime","type"),all = T)
+
+     plot_dt_final[,linesize := "b"]
+     plot_dt_final[type%in% c("totalpris","stotte"),linesize := "a"]
+
+     setcolorder(plot_dt_final,c("datetime","type","pris","lower_CI","upper_CI","linesize"))
+     plot_dt_final[,type:=factor(type,levels=c("spotpris","nettleie","stotte","totalpris"))]
+
+
+     setkey(plot_dt_final,datetime)
+
+
+     plot_dt_final
+   })
+
+
+   output$now_spotplot <- renderPlotly({
+     req(input$postnr,input$nettselskap, input$prisomraade)
+     if (identical(input$prisomraade, "")) return(NULL)
+     if (identical(input$nettselskap, "")) return(NULL)
+
+     plot_dt_final0 <- plot_dt_final()
+
+     p_now <- ggplot(data=plot_dt_final[datetime>=today-1],mapping=aes(x=datetime,y=pris,col=type,fill=type))+
+       geom_line(aes(size=linesize))+
+       geom_ribbon(aes(ymin = lower_CI, ymax = upper_CI), alpha = 0.5)+
+       #ggtitle("Estimert reell strømpris")+
+       scale_y_continuous(name = "NOK/kWh inkl. mva",labels=scaleFUN,breaks = breaks_extended(15))+
+       scale_x_datetime(name = "Tid/dato",
+                        breaks=breaks_pretty(12),
+                        minor_breaks = breaks_pretty(24),
+                        labels = label_date_short(format = c("%Y", "", "%d.%b\n", "%H:%M\n"),sep=""))+ # TODO: Get Norwegian months
+       scale_size_manual(values=c("a" = 1,"b"=0.5))+
+       guides(size="none")+
+       scale_color_manual(name="",values = mycols)+
+       scale_fill_manual(name="",values = mycols)+
+       geom_line(data=texthelper_dt[datetime>=today-1],aes(x=datetime,y=0,text=text),inherit.aes = F,size=0.00001)
+
+     ggp_now <- ggplotly(p_now,dynamicTicks = TRUE,tooltip = "text")
+     ggp_now <- layout(
+       ggp_now,
+       hovermode = "x unified"
+     )
+     ggp_now <- style(ggp_now,visible="legendonly",traces=c(3,7)) #trace=2 identified through plotly_json(ggp_now)
+     ggp_now <- style(ggp_now,hoverinfo="none",traces=1:8)
+     ggp_now
+
+   })
 
 
 
