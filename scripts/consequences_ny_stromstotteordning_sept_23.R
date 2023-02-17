23effa2f17acb0cb13ab4a03ad9e950047f80899
Martin <jullum@nr.no>
2023-02-17 06:48:58 +0100

    new computations

--- scripts/consequences_ny_stromstotteordning_sept_23.R
+++ scripts/consequences_ny_stromstotteordning_sept_23.R
@@ -3,17 +3,12 @@ library(data.table)
 library(ggplot2)
 Sys.setlocale(locale='nb_NO.utf8')
 
+# Doing everything before mva, and adding that in the end.
 
-area <- "NO1"
-
-org_comp_thres= 0.7*1.25
+org_comp_thres= 0.7
 org_comp_prop = 0.90
 
-dt_hourly <- fread("data/database_nordpool_hourly.csv")
-dt_comp <- fread("data/historic_estimated_compensation.csv")
-
-dt_comp[,max_estimation_date:=max(estimation_date),by=.(computation_year,computation_month)]
-meanprice_per_area <- dt_comp[estimation_date==max_estimation_date & type=="mean" & computation_year=="2022", .(area,mean_price,year=computation_year,month=computation_month)]
+dt_hourly <- fread("data/database_nordpool_hourly_2022_no_mva.csv")
 
 compfunc <- function(avgprice,compensation_threshold,compensation_prop){
   ifelse(avgprice<=compensation_threshold,
@@ -22,101 +17,35 @@ compfunc <- function(avgprice,compensation_threshold,compensation_prop){
 
 }
 
+dt_hourly[is.na(price),price:=0]
+
 dt_hourly[,year:=year(date)]
 dt_hourly[,month:=month(date)]
+dt_hourly[,mean_price:=mean(price),by=.(area,month)]
 
-dt_hourly <- dt_hourly[meanprice_per_area,on=c("area","year","month")]
-
-dt_hourly <- dt_hourly[year=="2022" & month>=10]
-
-(prop_of_price_compensated <- dt_hourly[area=="NO1",mean(price>=org_comp_thres)])
-#0.7955077
 
 dt_hourly[,org_comp:=compfunc(mean_price,org_comp_thres,org_comp_prop)]
 dt_hourly[,org_real_price:=price-org_comp]
 
-new_comp_thres1 <- 0.70*1.25
+new_comp_thres1 <- 0.70
 new_comp_prop1 <- 0.90
 
 dt_hourly[,new_comp1:=compfunc(price,new_comp_thres1,new_comp_prop1)]
 dt_hourly[,new_real_price1:=price-new_comp1]
 
-new_comp_thres2 <- 0.70*1.25
+new_comp_thres2 <- 0.70
 new_comp_prop2 <- 0.90*0.8935035 # kompensasjonsgrad 0.80 istedet for 0.90
 
 dt_hourly[,new_comp2:=compfunc(price,new_comp_thres2,new_comp_prop2)]
 dt_hourly[,new_real_price2:=price-new_comp2]
 
-new_comp_thres3 <- 0.70*1.2432324*1.25 # Innslag på 87 ør før moms (109 øre etter moms)
+new_comp_thres3 <- 0.70*1.2432324 # Innslag på 87 ør før moms (109 øre etter moms)
 new_comp_prop3 <- 0.90
 
 dt_hourly[,new_comp3:=compfunc(price,new_comp_thres3,new_comp_prop3)]
 dt_hourly[,new_real_price3:=price-new_comp3]
 
-
-
-
-aaa <- dt_hourly[,.(tot_org_comp=sum(org_comp),
-             sd_org_comp=sd(org_real_price),
-             range_org_comp=range(org_real_price),
-             tot_new_comp1=sum(new_comp1),
-             sd_new_comp1=sd(new_real_price1),
-             range_new_comp1=range(new_real_price1),
-             tot_new_comp2=sum(new_comp2),
-             sd_new_comp2=sd(new_real_price2),
-             range_new_comp2=range(new_real_price2),
-             tot_new_comp3=sum(new_comp3),
-             sd_new_comp3=sd(new_real_price3),
-             range_new_comp3=range(new_real_price3)),by=.(area,month)]
-
-dt_hourly[area=="NO1",.(tot_org_comp=sum(org_comp),
-             sd_org_comp=sd(org_real_price),
-             range_org_comp=range(org_real_price),
-             tot_new_comp1=sum(new_comp1),
-             sd_new_comp1=sd(new_real_price1),
-             range_new_comp1=range(new_real_price1),
-             tot_new_comp2=sum(new_comp2),
-             sd_new_comp2=sd(new_real_price2),
-             range_new_comp2=range(new_real_price2),
-             tot_new_comp3=sum(new_comp3),
-             sd_new_comp3=sd(new_real_price3),
-             range_new_comp3=range(new_real_price3))]
-
-
-
 dt_hourly[,tp:=as.POSIXct(date)+start_hour*60*60]
-
-dt_plot <- melt(dt_hourly[,.(area,
-                        tp,
-                        price,
-                        month,
-                        org_real_price,
-                        new_real_price1,
-                        new_real_price2,
-                        new_real_price3)], id.vars = c("area","month","tp"))
-
-
-ggplot(dt_plot,aes(x=tp,y=value,col=variable))+geom_line()+
-  facet_wrap(vars(area))
-
-
-ggplot(dt_plot[area=="NO1"],aes(x=tp,y=value,col=variable))+geom_line()+
-facet_wrap(vars(month),scales = "free_x",ncol = 1)
-
-ggplot(dt_plot[area=="NO1"& variable %in% c("price","org_real_price","new_real_price2","new_real_price3")],aes(x=tp,y=value,col=variable))+geom_line()+
-  facet_wrap(vars(month),scales = "free",ncol = 1)
-
-ggplot(dt_plot[area=="NO1"& variable %in% c("new_real_price2","new_real_price3")],aes(x=tp,y=value,col=variable))+geom_line()+
-  facet_wrap(vars(month),scales = "free",ncol = 1)
-
-ggplot(dt_plot[area=="NO1"& variable %in% c("org_real_price","new_real_price3")],aes(x=tp,y=value,col=variable))+geom_line()+
-  facet_wrap(vars(month),scales = "free",ncol = 1)
-
-ggplot(dt_plot[tp >= "2022-11-27" & tp < "2022-12-01" & area=="NO1"& variable %in% c("price","org_real_price","new_real_price3")],aes(x=tp,y=value,col=variable))+
-  geom_line()
-
-ggplot(dt_plot[tp >= "2022-12-24" & area=="NO1"& variable %in% c("price","org_real_price","new_real_price3")],aes(x=tp,y=value,col=variable))+
-  geom_line()
 
 
 
@@ -134,11 +63,122 @@ dt_dagsforbruk[,is_weekend:=(wday(dato) %in% c(1,7))]
 dt_timesforbruk_long <- rbind(dt_timesforbruk[,.(Time,andel_per_time=Prosentfordeling_helg,is_weekend=TRUE)],
                               dt_timesforbruk[,.(Time,andel_per_time=Prosentfordeling_ukedag,is_weekend=FALSE)])
 
-dt_forbruk <- merge(dt_dagsforbruk[dato>="2022-10-01",.(Prisomraade,dato,avg_forbruk_kWh,is_weekend)],dt_timesforbruk_long,by="is_weekend",allow.cartesian = T)
+dt_forbruk <- merge(dt_dagsforbruk[dato>="2022-01-01",.(Prisomraade,dato,avg_forbruk_kWh,is_weekend)],dt_timesforbruk_long,by="is_weekend",allow.cartesian = T)
 
 setcolorder(dt_forbruk,c("Prisomraade","Time","dato"))
 dt_forbruk[,forbruk_per_time:=avg_forbruk_kWh*andel_per_time]
 dt_forbruk[,tp:=as.POSIXct(dato)+(Time-1)*60*60]
+
+
+#### Computing cost with different compensation methods:
+
+dt_cost <- merge(dt_hourly[,.(area,tp,org_comp,org_real_price,
+                              new_comp1,new_real_price1,
+                              new_comp2,new_real_price2,
+                              new_comp3,new_real_price3)],
+                 dt_forbruk[,.(area=Prisomraade,tp,forbruk_per_time)],
+                 by=c("area","tp"))
+cols <- names(dt_cost)[-c(1,2,11)]
+dt_cost[area!="NO4",(cols):=lapply(.SD,function(x) x*1.25),.SDcols=cols]
+
+dt_cost[,consumer_cost_org := org_real_price*forbruk_per_time]
+dt_cost[,consumer_cost_new1 := new_real_price1*forbruk_per_time]
+dt_cost[,consumer_cost_new2 := new_real_price2*forbruk_per_time]
+dt_cost[,consumer_cost_new3 := new_real_price3*forbruk_per_time]
+
+dt_cost[,tot_comp_org := org_comp*forbruk_per_time]
+dt_cost[,tot_comp_new1 := new_comp1*forbruk_per_time]
+dt_cost[,tot_comp_new2 := new_comp2*forbruk_per_time]
+dt_cost[,tot_comp_new3 := new_comp3*forbruk_per_time]
+
+plot_dt <- dt_cost[,lapply(.SD,sum),.SDcols=c("consumer_cost_org","consumer_cost_new1",
+                                   "tot_comp_org","tot_comp_new1"),by=.(month(tp),area)]
+plot_dt[,diff_consumer_cost:=consumer_cost_org-consumer_cost_new1]
+plot_dt[,diff_tot_comp:=-(tot_comp_org-tot_comp_new1)]
+
+plot_dt_melt <- melt(plot_dt,id.vars = 1:2)
+
+
+ggplot(plot_dt_melt[variable%in%c("consumer_cost_org","consumer_cost_new1")],
+       aes(x=month,y=value,color=area,linetype=variable))+geom_line()
+
+ggplot(plot_dt_melt[variable%in%c("tot_comp_org","tot_comp_new1")],
+       aes(x=month,y=value,color=variable))+geom_line()+facet_wrap(vars(area),scales = "free")
+
+ggplot(plot_dt_melt[variable%in%c("tot_comp_new1")],
+       aes(x=month,y=value,color=area))+geom_line()
+
+ggplot(plot_dt_melt[variable%in%c("diff_tot_comp")],
+       aes(x=month,y=value,color=area))+geom_line()
+
+# prissvningner
+
+
+dt_plot2 <- dt_hourly[,.(area,date,start_hour,month,price,mean_price)]
+
+melt_dt_plot2 <- melt(dt_plot2,id.vars=1:4)
+
+ggplot(melt_dt_plot2,
+       aes(x=date,y=value,color=variable))+geom_line()+facet_wrap(vars(area),scales = "free")
+ggplot(melt_dt_plot2[area=="NO1"],
+       aes(x=date,y=value,color=variable))+geom_line()+facet_wrap(vars(area),scales = "free")
+
+### PS: HUSK Å KORRIGER ORG ORDNING DA DEN IKKE DEKKET SÅ MYE SOMD EN GJØR NÅ (90%)
+
+
+##### OLD ####
+
+
+dt_cost[,lapply(.SD,sum),.SDcols=c("consumer_cost_org","consumer_cost_new1","consumer_cost_new2","consumer_cost_new3",
+                                   "tot_comp_org","tot_comp_new1","tot_comp_new2","tot_comp_new3"),by=.(month(tp),area)]
+
+
+dt_cost[,lapply(.SD,sum),.SDcols=c("tot_comp_org","tot_comp_new1"),by=.(month(tp),area)]
+
+
+
+dt_cost[area=="NO1",lapply(.SD,sum),.SDcols=c("consumer_cost_org","consumer_cost_new1","consumer_cost_new2","consumer_cost_new3",
+                                              "tot_comp_org","tot_comp_new1","tot_comp_new2","tot_comp_new3")]
+
+(compensation_reduction_factor_org_new1 <- dt_cost[area=="NO1",sum(tot_comp_org)]/dt_cost[area=="NO1",sum(tot_comp_new1)])
+# 0.8935035
+
+
+dt_cost_melt <-  melt(dt_cost[,.(area,
+                                 tp,
+                                 consumer_cost_org,
+                                 consumer_cost_new1,
+                                 consumer_cost_new2,
+                                 consumer_cost_new3)], id.vars = c("area","tp"))
+
+
+ggplot(dt_cost_melt,aes(x=tp,y=value,col=variable))+geom_line()+
+  facet_wrap(vars(area))
+
+
+ggplot(dt_cost_melt[area=="NO1"],aes(x=tp,y=value,col=variable))+geom_line()+
+  facet_wrap(vars(month(tp)),scales = "free_x",ncol = 1)
+
+
+dt_cost[area=="NO1" & tp >="2022-12-25", sum((org_real_price+0.40)*forbruk_per_time)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##### #OLD ############
+
+
+
 
 
 ggplot(dt_forbruk[tp>"2022-12-10"],aes(x=tp,y=forbruk_per_time))+geom_line()+facet_wrap(vars(Prisomraade))
@@ -190,57 +230,6 @@ dt_cost_egen[tp > "2022-11-29" & tp <= "2022-12-01",sum(forbruk)]
 dt_cost_egen[tp > "2022-11-29" & tp <= "2022-12-01",sum(org_comp*forbruk)]
 
 
-#### Computing cost with different compensation methods:
-
-dt_cost <- merge(dt_hourly[,.(area,tp,org_comp,org_real_price,
-                              new_comp1,new_real_price1,
-                              new_comp2,new_real_price2,
-                              new_comp3,new_real_price3)],
-                 dt_forbruk[,.(area=Prisomraade,tp,forbruk_per_time)],
-                 by=c("area","tp"))
-dt_cost[,consumer_cost_org := org_real_price*forbruk_per_time]
-dt_cost[,consumer_cost_new1 := new_real_price1*forbruk_per_time]
-dt_cost[,consumer_cost_new2 := new_real_price2*forbruk_per_time]
-dt_cost[,consumer_cost_new3 := new_real_price3*forbruk_per_time]
-
-dt_cost[,tot_comp_org := org_comp*forbruk_per_time]
-dt_cost[,tot_comp_new1 := new_comp1*forbruk_per_time]
-dt_cost[,tot_comp_new2 := new_comp2*forbruk_per_time]
-dt_cost[,tot_comp_new3 := new_comp3*forbruk_per_time]
-
-
-dt_cost[,lapply(.SD,sum),.SDcols=c("consumer_cost_org","consumer_cost_new1","consumer_cost_new2","consumer_cost_new3",
-                                   "tot_comp_org","tot_comp_new1","tot_comp_new2","tot_comp_new3"),by=.(month(tp),area)]
-
-
-dt_cost[,lapply(.SD,sum),.SDcols=c("tot_comp_org","tot_comp_new1"),by=.(month(tp),area)]
-
-
-
-dt_cost[area=="NO1",lapply(.SD,sum),.SDcols=c("consumer_cost_org","consumer_cost_new1","consumer_cost_new2","consumer_cost_new3",
-                                              "tot_comp_org","tot_comp_new1","tot_comp_new2","tot_comp_new3")]
-
-(compensation_reduction_factor_org_new1 <- dt_cost[area=="NO1",sum(tot_comp_org)]/dt_cost[area=="NO1",sum(tot_comp_new1)])
-# 0.8935035
-
-
-dt_cost_melt <-  melt(dt_cost[,.(area,
-                             tp,
-                             consumer_cost_org,
-                             consumer_cost_new1,
-                             consumer_cost_new2,
-                             consumer_cost_new3)], id.vars = c("area","tp"))
-
-
-ggplot(dt_cost_melt,aes(x=tp,y=value,col=variable))+geom_line()+
-  facet_wrap(vars(area))
-
-
-ggplot(dt_cost_melt[area=="NO1"],aes(x=tp,y=value,col=variable))+geom_line()+
-  facet_wrap(vars(month(tp)),scales = "free_x",ncol = 1)
-
-
-dt_cost[area=="NO1" & tp >="2022-12-25", sum((org_real_price+0.40)*forbruk_per_time)]
 
 
 #### Plott til innlegg:
